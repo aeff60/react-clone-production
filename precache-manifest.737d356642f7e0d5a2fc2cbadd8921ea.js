ebb7513abe9345ced541d948ef428a4426944d89
Sirasit Boonklang <46419278+aeff60@users.noreply.github.com>
2020-11-01 19:43:23 +0700

    Add files via upload

---
+++ precache-manifest.737d356642f7e0d5a2fc2cbadd8921ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd9d84a0e4e30eb4389a31a56b82577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef7781ca928377e6c2f",
+    "url": "/static/css/main.7bcc1bc2.chunk.css"
+  },
+  {
+    "revision": "240e413da0035c3716f9",
+    "url": "/static/js/2.29234ef1.chunk.js"
+  },
+  {
+    "revision": "7ef7781ca928377e6c2f",
+    "url": "/static/js/main.12281f18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
